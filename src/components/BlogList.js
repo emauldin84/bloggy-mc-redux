b02f94bb4b6f2e38a5f14e60a26f95fbc080521b
Eric Mauldin <emauldin84@gmail.com>
2019-05-09 11:58:00 -0400

    updatest the BlogList so it shows the blog posts and ids

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function BlogList({posts}) {
     // helper function implicitly returns a <p>
-    const listItems = Object.values(posts).map(post => <p>{post.title}</p>);
+    // const listItems = Object.values(posts).map(post => <p>{post.title}</p>);
+
+    // if we want the IDs as well as the blog posts,
+    // we should get an array of teh IDs (using Object.keys)
+    // Then, we map over that array.
+    // With each id, we can access the blog post in the posts object.
+    const listItems = Object.keys(posts).map(id => {
+        const theBlogPost = posts[id];
+        return <p>{theBlogPost.title} - id:{id}</p>
+    })
     return(
         <ul>
             {listItems}
