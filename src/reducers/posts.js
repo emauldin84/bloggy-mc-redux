f71ec84d3bcc030ffc593e57623a1942dff04a5d
Eric Mauldin <emauldin84@gmail.com>
2019-05-09 09:58:48 -0400

    adds store that uses posts reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,6 +1,6 @@
 import { ACTION_CREATE_POST } from '../actions'
 // A reducer is a function that accepts the current state and an action. Then, calculates and returns the nest new version of state
-export function posts(state={}, action={type: ''}) {
+export default function posts(state={}, action={type: ''}) {
     switch(action.type) {
         case ACTION_CREATE_POST:
         // create a post
---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { combineReducers, createStore } from 'redux';
+
+import posts from './reducers/posts';
+
+// Pass an object to combinedReducers
+// This object should be shaped like your state
+const rootReducer = combinedReducers({
+    posts,
+})
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+export default store;
